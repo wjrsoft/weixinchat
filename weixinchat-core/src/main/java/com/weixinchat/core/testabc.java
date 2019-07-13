4178173dfe206b481e65525200236efe16d2c301
wjrsoft <wjrsoftware@163.com>
2019-07-14 02:04:04 +0800

    冲突来了

--- weixinchat-core/src/main/java/com/weixinchat/core/testabc.java
+++ weixinchat-core/src/main/java/com/weixinchat/core/testabc.java
@@ -2,6 +2,6 @@ package com.weixinchat.core;
 
 public class testabc {
 	public static void main(String[] args) {
-		System.out.println("新的dev分支,修改会有1111冲突吗1");
+		System.out.println("新的dev分支,同一个分支，冲突来了");
 	}
 }
