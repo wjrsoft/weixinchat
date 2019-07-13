1f162defc4daaf3f38211e0aec1494157d71b4ae
wjrsoft <wjrsoftware@163.com>
2019-07-14 01:53:41 +0800

    111

--- weixinchat-core/src/main/java/com/weixinchat/core/testabc.java
+++ weixinchat-core/src/main/java/com/weixinchat/core/testabc.java
@@ -2,6 +2,6 @@ package com.weixinchat.core;
 
 public class testabc {
 	public static void main(String[] args) {
-		System.out.println("新的dev分支,修改会有冲突吗1");
+		System.out.println("新的dev分支,修改会有1111冲突吗1");
 	}
 }
