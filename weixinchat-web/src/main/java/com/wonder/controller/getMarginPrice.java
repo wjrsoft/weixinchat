d244d065ec52977397f3e427f2a2a69a0e40f903
wangjinrong <wangjinrong@192.168.0.107>
2019-10-06 23:23:08 +0800

    spring controll

--- weixinchat-web/src/main/java/com/wonder/controller/getMarginPrice.java
+++ weixinchat-web/src/main/java/com/wonder/controller/getMarginPrice.java
@@ -3,7 +3,8 @@ package com.wonder.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +19,7 @@ import com.wonder.utils.HttpClientUtil;
  */
 @Controller
 public class getMarginPrice {
-	private static Logger log = Logger.getLogger(getMarginPrice.class);
+	private final static Logger logger = LoggerFactory.getLogger(getMarginPrice.class);
 	@RequestMapping("/getMarginPrice.do")
 	@ResponseBody
 	public JSONArray hello(HttpServletRequest req, HttpServletResponse resp) {
@@ -30,15 +31,15 @@ public class getMarginPrice {
 			json = HttpClientUtil.doGet(
 					"http://www.chinamoney.com.cn/r/cms/www/chinamoney/data/fx/cpair-quot.json?t=1562509287483&t=1562509297494'");
 		} catch (Exception e) {
-			log.error(e);
-		}
+			logger.error(e.getMessage());
+			}
 		return JSON.parseArray(getPrice(json));
 	}
 	
 	public String getPrice(String json) {
 		JSONObject jsonObject = JSONObject.parseObject(json);
 		Object obj = jsonObject.get("records");
-		System.out.println(JSON.toJSONString(obj));
+		logger.info(JSON.toJSONString(obj));
 		return JSON.toJSONString(obj);
 	}
 
